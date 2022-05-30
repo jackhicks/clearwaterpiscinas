cae276a3679457ba64ce6722026657ebc80619d4
Jack <hicksdjack@gmail.com>
2022-05-30 18:19:46 +0200

    Add link to partners

--- src/content/clientLogos.ts
+++ src/content/clientLogos.ts
@@ -1,5 +1,8 @@
 export const clientLogos = [
   {
+    alt: 'www.clearskyvillas.com',
+    link: 'https://www.clearskyvillas.com',
     image:
       'https://www.clearskyvillas.com/wp-content/uploads/2015/10/light-blue-1.png',
-  }];
+  },
+];
