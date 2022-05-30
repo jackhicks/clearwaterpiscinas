4d8361cedfa25bb7a2411f358e7c174e016a3bec
Jack <hicksdjack@gmail.com>
2022-05-30 18:20:25 +0200

    Add link to partners

--- src/components/Clients/index.tsx
+++ src/components/Clients/index.tsx
@@ -2,16 +2,18 @@ import { clientLogos } from '../../content/clientLogos';
 
 export const Clients = () => {
   return (
-    <section className="p-16">
-      <h1 className=" mb-24">Selected partners</h1>
+    <section className="pt-16 px-16   ">
+      <h1 className=" mb-16">Selected partners</h1>
       <div className="grid grid-cols-4">
         {clientLogos.map(logo => (
           <div className="mb-16 flex" key={logo?.image}>
-            <img
-              src={logo?.image}
-              alt=""
-              className="w-48 sm:w-32 lg:w-48 select-none pointer-events-none"
-            />
+            <a target="_blank" href={logo?.link}>
+              <img
+                src={logo?.image}
+                alt={logo?.alt}
+                className="w-48 sm:w-32 lg:w-48 select-none pointer-events-none"
+              />
+            </a>
           </div>
         ))}
       </div>
