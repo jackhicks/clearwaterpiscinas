58fb63825e55aa81ef4348b10c311e2db77cd617
Jack <hicksdjack@gmail.com>
2022-07-18 19:13:34 +0200

    changed menu and added burger menu

---
+++ src/components/Button/Back.tsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+
+export const BackButton = () => {
+  return (
+    <>
+      {props.location}
+      <Link className="p-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="#24549b"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M10 19l-7-7m0 0l7-7m-7 7h18"
+          />
+        </svg>
+      </Link>
+    </>
+  );
+};
