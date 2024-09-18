407151f0d32dd3e7c51d4bd2eaf659764221a9ad
Jack <hicksdjack@gmail.com>
2024-09-18 20:05:53 +0200

    Include product specs

--- src/components/ProductItem/index.tsx
+++ src/components/ProductItem/index.tsx
@@ -3,32 +3,29 @@ import { useParams, Navigate } from 'react-router-dom';
 import { BackButton } from '../Button/Back';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button } from '../Button';
 
 export const ProductItem = () => {
   const { id } = useParams();
-  const filteredItems = ProductContentItems.filter(item => item.pageLink == id);
-  const { t, i18n } = useTranslation();
+  const filteredItems = ProductContentItems.filter(
+    item => item.pageLink === id,
+  );
+  const { t } = useTranslation();
 
   useEffect(() => {
     document.title = t('titleShort') + ' - ' + id;
-  });
-
-  const buyButtonHandle = () => {
-    alert('Do something');
-  };
+  }, [id, t]);
 
   return (
     <>
       <div className="bg-white">
         <BackButton />
       </div>
-      <div className="bg-white pt-8 px-16 flex flex-col ">
-        {filteredItems.length == 0 ? (
+      <div className="bg-white pt-8 px-16 flex flex-col">
+        {filteredItems.length === 0 ? (
           <Navigate to="/Products" />
         ) : (
           filteredItems.map(item => (
-            <div>
+            <>
               <section
                 key={item.pageLink}
                 className="flex flex-col md:flex-row text-s font-serif"
@@ -37,7 +34,7 @@ export const ProductItem = () => {
                   <img
                     className="max-h-[300px] max-w-[300px] "
                     src={item.image}
-                    alt="item"
+                    alt={t(item.type + '.' + item.pageLink + '.title')}
                   />
                 </div>
                 <div className="mb-4 flex-col basis-full md:basis-1/2 text-blue font-serif">
@@ -45,50 +42,34 @@ export const ProductItem = () => {
                     {t(item.type + '.' + item.pageLink + '.title')}
                   </h1>
                   <div className="md:mx-8 mb-8 text-justify text-blue font-serif">
-                    <p>
-                      {t(item.type + '.' + item.pageLink + '.shortDescription')}
-                    </p>
+                    <p>{item.description}</p>
                   </div>
 
-                  <div className="BuyPrice flex flex-row md:mx-8 mb-8 place-content-between text-justify text-blue font-serif">
-                    <div>
-                      <p>{item.details.price}€</p>
-                    </div>
-                    <Button onClick={buyButtonHandle}>
-                      {t('Buttons.BuyNow')}
-                    </Button>
+                  {/* Price and Buy Now Section */}
+                  <div className="md:mx-8 mb-8 flex items-center space-x-4">
+                    <span className="text-2xl font-bold">
+                      {t('Price')}: {item.price} €
+                    </span>
                   </div>
                 </div>
               </section>
-              <section
-                key={item.pageLink}
-                className="flex flex-col text-s font-serif"
-              >
-                <div>
-                  <h1 className="md:mx-8 mb-8 text-xl text-blue font-serif">
-                    {t('pageStructure.DetailedDescription')}
-                  </h1>
+
+              <div className="mb-4 flex-col basis-full md:basis-1/2 text-blue font-serif">
+                {/* Specifications Section */}
+                {item.specifications && item.specifications.length > 0 && (
                   <div className="md:mx-8 mb-8 text-justify text-blue font-serif">
-                    <p>{t(item.type + '.' + item.pageLink + '.description')}</p>
+                    <h2 className="text-2xl mb-4">{t('Specifications')}</h2>
+                    <ul className="list-disc pl-5">
+                      {item.specifications.map((spec, index) => (
+                        <li key={index} className="mb-2">
+                          <strong>{spec.name}:</strong> {spec.value}
+                        </li>
+                      ))}
+                    </ul>
                   </div>
-                </div>
-                <div>
-                  <h1 className="md:mx-8 mb-8 text-xl text-blue font-serif">
-                    {t('pageStructure.TechDetailsName')}
-                  </h1>
-                  <div className="md:mx-8 mb-8 text-justify text-blue font-serif">
-                    {Object.keys(item.details.tech).length == 0
-                      ? null
-                      : Object.keys(item.details.tech).map(TechItem => (
-                          <p>
-                            {t('pageStructure.TechDetails.' + TechItem)}:{' '}
-                            {item.details.tech[TechItem]}
-                          </p>
-                        ))}
-                  </div>
-                </div>
-              </section>
-            </div>
+                )}
+              </div>
+            </>
           ))
         )}
       </div>
